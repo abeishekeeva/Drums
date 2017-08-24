a18e9ecdc4b22cd6101d074a3509e582fcc4c6a4
abiesh <aidai.beishekeeva@gmail.com>
2017-08-24 16:45:00 +0600

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+function playAudio(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+
+window.addEventListener('keydown', playAudio);
